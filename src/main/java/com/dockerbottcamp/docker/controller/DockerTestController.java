f5f595ec07330ce32f558d3f3eea76a528c8f5b3
Fennydude <bogurov1@gmail.com>
2024-10-23 12:09:57 +0200

    5. add docker-compose.yml update code base to work with DB

--- src/main/java/com/dockerbottcamp/docker/controller/DockerTestController.java
+++ src/main/java/com/dockerbottcamp/docker/controller/DockerTestController.java
@@ -1,10 +1,14 @@
 package com.dockerbottcamp.docker.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
@@ -13,8 +17,17 @@ public class DockerTestController {
     @Value("${env.devops}")
     private String devops;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/get-docker")
     private String getDocker(){
         return "Hello world with auto push 2.0 and devops " + devops;
+    }
+
+    @GetMapping("/get-sample-data")
+    public List<String> getSampleData() {
+        String query = "SELECT name FROM sample_table";
+        return jdbcTemplate.query(query, (rs, rowNum) -> rs.getString("name"));
     }
 }
